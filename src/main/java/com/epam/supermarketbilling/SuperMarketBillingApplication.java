558d31fd4f128274448cd80be69e9e9614d30beb
yaswant <yaswanthero123@gmail.com>
2023-05-01 18:21:23 -0700

    Initial commit

---
+++ src/main/java/com/epam/supermarketbilling/SuperMarketBillingApplication.java
@@ -0,0 +1,13 @@
+package com.epam.supermarketbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMarketBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMarketBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/supermarketbilling/SuperMarketBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.supermarketbilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
